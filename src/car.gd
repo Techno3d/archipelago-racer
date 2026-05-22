a0d640a059b32bbbb6147ae82bf271461f591a46
Aaron J <jamesacs329@gmail.com>
2026-05-22 14:10:49 -0400

    player controls

--- src/car.gd
+++ src/car.gd
@@ -1,24 +1,40 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+var SPEED = 0.0
+var accel = 40.0
+var friction = 1.0
+var top_speed = 200
 
+var turn_speed = 2.5
+var current_turn = 0.0
+var turn_friction = 4.0  
+var max_turn = 1.0
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	print("SPEED: ", SPEED, " | velocity: ", velocity)
+	
+	
+	if Input.is_action_pressed("accel"):
+		SPEED = clamp(SPEED + accel * delta,0, top_speed)
+	elif Input.is_action_pressed("reverse"):
+		SPEED = clamp(SPEED - accel * delta,-top_speed * 0.5, 0)
+	else:
+		SPEED = move_toward(SPEED,0,friction)
+		
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("left", "right", "accel", "reverse")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	var turn_input = Input.get_axis("left", "right")
+	current_turn = move_toward(current_turn, turn_input * max_turn, turn_friction * delta)
+
+	var speed_factor = clamp(abs(SPEED) / top_speed, 0.0, 1.0)
+	rotate_y(-current_turn * turn_speed * speed_factor * delta)
+	
+		
+	velocity.x = -transform.basis.z.x * SPEED
+	velocity.z = -transform.basis.z.z * SPEED
+		
 
 	move_and_slide()
