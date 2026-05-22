7f8745d8d5a9e7fbb9b7e12bf6ba3919057fdc24
Aaron J <jamesacs329@gmail.com>
2026-05-22 19:04:56 -0400

    water`

--- src/car.gd
+++ src/car.gd
@@ -13,9 +13,26 @@ var max_turn = 1.0
 @export var accel_curve: Curve
 @onready var car_model: CarModel = $CarMesh
 
+func _ready() -> void:
+	if Archipelago.conn:
+		Archipelago.conn.deathlink.connect(respawn)
+
 func _physics_process(delta: float) -> void:
 	print("SPEED: ", SPEED, " | velocity: ", velocity)
 	car_model.speed = SPEED
+
+	var tb: TerraBrush = $"../TerraBrush"
+	
+	# Let's assume we have a variable called "_terraBrush" that is a TerraBrush Node.
+	# Let's validate that we are on the ground
+
+	
+	if tb:
+		interact_terrain(tb)
+	
+	
+	#print("SPEED: ", SPEED, " | velocity: ", velocity)
+
 	
 	
 	var throttle = Input.get_axis("reverse", "accel");
@@ -24,8 +41,7 @@ func _physics_process(delta: float) -> void:
 	else:
 		SPEED = clampf(SPEED + throttle * accel * accel_curve.sample(SPEED/top_speed) * delta, -top_speed/2., top_speed)
 
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	
 
 
 	var turn_input = Input.get_axis("left", "right")
@@ -39,5 +55,39 @@ func _physics_process(delta: float) -> void:
 	velocity.x = -transform.basis.z.x * SPEED
 	velocity.z = -transform.basis.z.z * SPEED
 		
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 
 	move_and_slide()
+
+# handles interaction points and respawning
+func interact_terrain(tb: TerraBrush):
+	if is_on_floor() and get_last_slide_collision() != null:
+			print("1")
+			# Get the current collision
+			var collision = get_last_slide_collision()
+
+			# Let's make sure the collider is the collider of the Terrain
+			if collision.get_collider() == tb.getTerrainCollider():
+				print("2")
+				# The variable playerX and playerZ are the variables for the global position of the player
+				var result = tb.getPositionInformation(global_position.x, global_position.z)
+				tb.addInteractionPoint(global_position.x,global_position.z)
+				# If we don't get a res*ult, it means we are out of the terrain
+				if result != null:
+					# The texture at position 0 is the most present one
+					var _mainTexture = result.get_textures()[0].get_name() if result.get_textures().size() > 0 else ""
+					print("res:", result.get_waterDeepness(), result.get_waterFactor())
+
+					if result.get_waterDeepness() > 1.2:
+						respawn()
+						print("respawning!")
+
+					
+				else:
+					print("???")
+
+			
+
+func respawn():
+	pass
