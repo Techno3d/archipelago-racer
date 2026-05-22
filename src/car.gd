50be3f5076b439b5996183cb1c14152089b9203e
Shadman Syed <shadman3d@gmail.com>
2026-05-22 16:36:12 -0400

    some small car changes

--- src/car.gd
+++ src/car.gd
@@ -1,27 +1,27 @@
 extends CharacterBody3D
 
-
 var SPEED = 0.0
-var accel = 40.0
+var accel = 50.0
 var friction = 1.0
 var top_speed = 200
 
-var turn_speed = 2.5
+var turn_speed = 0.5
 var current_turn = 0.0
 var turn_friction = 4.0  
 var max_turn = 1.0
+
+@export var accelCurve: Curve
 
 func _physics_process(delta: float) -> void:
 	print("SPEED: ", SPEED, " | velocity: ", velocity)
 	
 	
-	if Input.is_action_pressed("accel"):
-		SPEED = clamp(SPEED + accel * delta,0, top_speed)
-	elif Input.is_action_pressed("reverse"):
-		SPEED = clamp(SPEED - accel * delta,-top_speed * 0.5, 0)
-	else:
+	var throttle = Input.get_axis("reverse", "accel");
+	if throttle == 0:
 		SPEED = move_toward(SPEED,0,friction)
-		
+	else:
+		SPEED = clampf(SPEED + throttle * accel * accelCurve.sample(SPEED/top_speed) * delta, -top_speed/2., top_speed)
+
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
@@ -29,7 +29,7 @@ func _physics_process(delta: float) -> void:
 	var turn_input = Input.get_axis("left", "right")
 	current_turn = move_toward(current_turn, turn_input * max_turn, turn_friction * delta)
 
-	var speed_factor = clamp(abs(SPEED) / top_speed, 0.0, 1.0)
+	var speed_factor = clamp(SPEED / top_speed, -0.5, 1.0)
 	rotate_y(-current_turn * turn_speed * speed_factor * delta)
 	
 		
