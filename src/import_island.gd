1117740ab807f508439ed0df702cb5ef301ab28d
Shadman Syed <shadman3d@gmail.com>
2026-05-24 14:11:42 -0400

    islands come with trimesh collider

---
+++ src/import_island.gd
@@ -0,0 +1,20 @@
+@tool # Needed so it runs in editor.
+extends EditorScenePostImport
+
+# This sample changes all node names.
+# Called right after the scene is imported and gets the root node.
+func _post_import(scene):
+	# Change all node names to "modified_[oldnodename]"
+	iterate(scene)
+	return scene # Remember to return the imported scene
+
+# Recursive function that is called on every node
+# (for demonstration purposes; EditorScenePostImport only requires a `_post_import(scene)` function).
+func iterate(node):
+	if node != null:
+		if node is MeshInstance3D:
+			(node as MeshInstance3D).create_trimesh_collision()
+		print_rich("Post-import: [b]%s[/b] -> [b]%s[/b]" % [node.name, "modified_" + node.name])
+		node.name = "modified_" + node.name
+		for child in node.get_children():
+			iterate(child)
