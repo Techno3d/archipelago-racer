22373080f5d84a7c6176433cd6f36ed8eefe5c57
Shadman Syed <shadman3d@gmail.com>
2026-06-02 11:48:32 -0400

    working suspension

--- src/vehicular_car.gd
+++ src/vehicular_car.gd
@@ -16,29 +16,43 @@ var spawn_point: Transform3D
 @export var top_speed: float = 35
 @export var power_steering_factor: float = 2.1
 @export var air_damp: float = 0.05
-@export var mu_k: float = 0.7
+@export var mu_k: float = 0.8
 @export var mu_s: float = 0.9
 @onready var car_model: CarModel = $Car
 @onready var wheel_base: Area3D = $base
 @onready var steering_point: Node3D = $SteeringPoint
 @onready var cam_pivot: Node3D = $CamPivot
-@onready var cam_point: Marker3D = $CamPoint
+
+# Suspension
+@export var spring_constant = 80000
+@export_range(0, 3, .01) var damp_ratio = 0.9
+var damp_constant
+@export var rest_dist = 0.44
+@onready var fl_raycast: RayCast3D = $FLRay
+@onready var rl_raycast: RayCast3D = $RLRay
+@onready var fr_raycast: RayCast3D = $FRRay
+@onready var rr_raycast: RayCast3D = $RRRay
+var raycasts: Array[RayCast3D]
 
 # Car Wheel Contact Points
 @onready var fl_contact: Marker3D = $FLContact
 @onready var rl_contact: Marker3D = $RLContact
+@onready var fr_contact: Marker3D = $FRContact
+@onready var rr_contact: Marker3D = $RRContact
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	wheel_base.body_entered.connect(on_ground)
 	wheel_base.body_exited.connect(on_ground)
+	damp_constant = 2*sqrt(spring_constant*mass)*damp_ratio/9
+	raycasts = [fl_raycast, rl_raycast, fr_raycast, rr_raycast]
 	spawn_point = transform
 	if Archipelago.conn:
 		Archipelago.conn.deathlink.connect(respawn)
 
 func on_ground(_body: Node3D):
 	if wheel_base.has_overlapping_bodies():
-		linear_damp = 1
+		linear_damp = 0.8
 		gravity_scale = 3.0
 	else:
 		linear_damp = air_damp
@@ -60,6 +74,21 @@ func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 		state.apply_torque((global_basis*Vector3.RIGHT).normalized()*air_factor*pitch_input*mass)
 		state.apply_torque(up_axis*air_factor*-turn_input*mass)
 		return
+
+	# Suspension
+	for raycast in raycasts:
+		raycast.force_raycast_update()
+		if raycast.is_colliding():
+			var contact = raycast.get_collision_point()
+			var spring_len = raycast.global_position.distance_to(contact)
+			# (raycast.get_child(0) as Marker3D).position.y = -spring_len
+			var spring_up = raycast.global_basis.y
+			# The x in F=-kx
+			var offset = rest_dist - spring_len
+			var point_vel = state.linear_velocity + state.angular_velocity.cross(contact - global_position)
+			# var damped_spring_force = (offset*spring_constant - damp_constant*(state.get_velocity_at_local_position(raycast.position).dot(spring_up)))*raycast.get_collision_normal()
+			var damped_spring_force = (offset*spring_constant - damp_constant*(point_vel.dot(spring_up)))*raycast.get_collision_normal()
+			state.apply_force(damped_spring_force, contact - global_position)
 	
 	# Ground controls
 	var forward_dir: Vector3 = (global_transform.basis * Vector3(0,0,1)).normalized()
@@ -76,45 +105,44 @@ func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 	var up_ramp = get_gravity().cross(global_down).cross(global_down).normalized()
 	# Cross product here is for sin(theta)
 	var tangential_gravity = abs((mass * get_gravity().cross(global_down).length() * -up_ramp).dot(global_right))
-	var fudge_factor = (state.linear_velocity*Vector3(1,0.1,1)).length()/top_speed*max_static_friction*1.036 + state.angular_velocity.length()/top_angular_speed*max_static_friction*1.04
-	if (tangential_gravity + fudge_factor) < max_static_friction:
+	if (tangential_gravity) < max_static_friction:
 		# Static Friction
 		state.apply_central_force(up_ramp*tangential_gravity)
-	else:
-		# Kinetic Friction
-		var fraction_linear_cross_front = (perp_wheel_dir).dot(linear_velocity)
-		var fraction_linear_cross_back = (global_basis*Vector3.RIGHT).dot(linear_velocity)
-		state.apply_central_force(perp_wheel_dir*fraction_linear_cross_front*-1*mass*mu_k)
-		state.apply_central_force((global_basis*Vector3.RIGHT)*fraction_linear_cross_back*-1*mass*mu_k)
+	# Technically kinetic should only be if no static, but I don't wanna calculate static friction with velocity, I just wanna cancel gravity
+	# Kinetic Friction
+	var fraction_linear_cross_front = perp_wheel_dir.dot(state.linear_velocity)
+	var fraction_linear_cross_back = global_basis.x.dot(state.linear_velocity)
+	state.apply_central_force(perp_wheel_dir*fraction_linear_cross_front*-1*mass*mu_k)
+	state.apply_central_force(global_basis.x*fraction_linear_cross_back*-1*mass*mu_k)
 
 
 	# Torque should be r x F, where r is the position offset from CM to force, and F is the force.
 	var steer_dir = (steering_point.global_position - global_position).normalized().cross(wheel_dir)
 	# Not physics based turning force magnitude
-	var steer_torque = steer_dir * mass * 4 * clampf(linear_velocity.length()/(top_speed/3), 0, 1) * (-1 if breaking > throttle else 1)
+	var steer_torque = steer_dir * mass * 2 * clampf(state.linear_velocity.length()/(top_speed/2), 0, 1) * (-1 if breaking > throttle else 1)
 	state.apply_torque(steer_torque)
 
 	# Mass times accel * direction of wheel * where on accel curve. Power of engine/motor is limited, but it takes more power the faster you are to move
 	# as `P = F * v` I think. This also serves to clamp the speed
 	# Factor of 100 is there to make it work, idk why it doesn't without it
-	var motor_force = forward_dir * throttle * accel * mass * accel_curve.sample(clampf(linear_velocity.length()/top_speed, 0, 1)) * 100
-	var breaking_force = -forward_dir * breaking * accel/2. * mass * accel_curve.sample(clampf(linear_velocity.length()/(top_speed/2.), 0, 1)*-1) * 100
+	var motor_force = forward_dir * throttle * accel * mass * accel_curve.sample(clampf(state.linear_velocity.length()/top_speed, 0, 1)) * 100
+	var breaking_force = -forward_dir * breaking * accel/2. * mass * accel_curve.sample(clampf(state.linear_velocity.length()/(top_speed/2.), 0, 1)*-1) * 100
 
-	var turn_quat_lower = Quaternion.from_euler(Vector3.UP * turn_input * TAU/180.*power_steering_factor * turn_curve.sample(angular_velocity.length()/top_angular_speed * linear_velocity.length()/top_speed)).normalized()
+	var turn_quat_lower = Quaternion.from_euler(Vector3.UP * turn_input * TAU/180.*power_steering_factor * turn_curve.sample(state.angular_velocity.length()/top_angular_speed * state.linear_velocity.length()/top_speed)).normalized()
 	var less_wheel_dir = ((global_transform.basis * (Vector3(0,0,1) * turn_quat_lower)).normalized())
-	var motor_force_steer = less_wheel_dir * throttle * accel * mass * accel_curve.sample(clampf(linear_velocity.length()/top_speed, 0, 1)) * 100
-	var breaking_force_steer = -less_wheel_dir * breaking * accel/2. * mass * accel_curve.sample(clampf(linear_velocity.length()/(top_speed/2.), 0, 1)*-1) * 100
+	var motor_force_steer = less_wheel_dir * throttle * accel * mass * accel_curve.sample(clampf(state.linear_velocity.length()/top_speed, 0, 1)) * 100
+	var breaking_force_steer = -less_wheel_dir * breaking * accel/2. * mass * accel_curve.sample(clampf(state.linear_velocity.length()/(top_speed/2.), 0, 1)*-1) * 100
 
 	# Using the actual turn on the front wheel cars usually ends in weird jumping behavior
 	# Most of the torque should already be calculated above, this is just for extra turn
 	state.apply_force(motor_force_steer/4., global_basis*fl_contact.position)
-	state.apply_force(motor_force_steer/4., global_basis*(fl_contact.position*Vector3(-1,1,1)))
+	state.apply_force(motor_force_steer/4., global_basis*fr_contact.position)
 	state.apply_force(motor_force/4., global_basis*rl_contact.position)
-	state.apply_force(motor_force/4., global_basis*(rl_contact.position*Vector3(-1,1,1)))
+	state.apply_force(motor_force/4., global_basis*rr_contact.position)
 	state.apply_force(breaking_force_steer/4., global_basis*fl_contact.position)
-	state.apply_force(breaking_force_steer/4., global_basis*(fl_contact.position*Vector3(-1,1,1)))
+	state.apply_force(breaking_force_steer/4., global_basis*fr_contact.position)
 	state.apply_force(breaking_force/4., global_basis*rl_contact.position)
-	state.apply_force(breaking_force/4., global_basis*(rl_contact.position*Vector3(-1,1,1)))
+	state.apply_force(breaking_force/4., global_basis*rr_contact.position)
 
 func _process(_delta: float) -> void:
 	car_model.speed = linear_velocity.length()
