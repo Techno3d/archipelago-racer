df61d2b6847ed4e309d003aab88d691a77bc1f5c
Shadman Syed <shadman3d@gmail.com>
2026-06-06 20:59:06 -0400

    you can pause now

--- ui.gd
+++ ui.gd
@@ -5,10 +5,12 @@ extends CanvasLayer
 @onready var best_time_label: Label = %BestTime
 @onready var checkpoint_label: Label = %Checkpoint
 @onready var restart_label: Label = $MarginContainer/MainContainer/Bottom/Restart
+@onready var pause_container: MarginContainer = $MarginContainer/PauseContainer
 var car: VehicularCar
 
 func _ready() -> void:
 	restart_label.hide()
+	pause_container.hide()
 	car = get_tree().get_first_node_in_group("PlayerCar")
 	if car != null:
 		car.restart_hint.connect(func():
@@ -16,6 +18,15 @@ func _ready() -> void:
 			await get_tree().create_timer(4).timeout
 			restart_label.hide()
 		)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_released("pause"):
+		if !get_tree().paused:
+			get_tree().paused = true
+			pause_container.show()
+		else:
+			get_tree().paused = false
+			pause_container.hide()
 
 func _process(_delta: float) -> void:
 	timer_label.text = "%.2fs" % [Globals.goal.current_run_timer]
