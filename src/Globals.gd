8763362509962df7d907e05a86862d4da25dc473
Shadman Syed <shadman3d@gmail.com>
2026-06-08 23:03:16 -0400

    fix ui being wonky and add insane stats

--- src/Globals.gd
+++ src/Globals.gd
@@ -8,8 +8,10 @@ signal set_max_checkpoints(num: int)
 
 var goal: TimeTrials
 var car_stats: Array[CarStats] = [
+    preload("res://car_stats/InsaneStats.tres"),
     preload("res://car_stats/MainStats.tres"),
+    preload("res://car_stats/MidStats.tres"),
     preload("res://car_stats/StarterStats.tres"),
 ]
 ## Index into previous array (maybe not the best solution?)
-var current_stat = 0
+var current_stat = 1
